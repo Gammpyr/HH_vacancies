4d0b832f702b108fcb8679231951365d8c4daa22
Gammpyr <malak93@yandex.ru>
2025-06-13 14:40:35 +0300

    add save_to_file.py

---
+++ src/save_to_file.py
@@ -0,0 +1,10 @@
+from src.vacancy import Vacancy
+
+
+class JSONSaver:
+    """Класс для сохранения в JSON-файл"""
+    def add_vacancy(self, vacancy: Vacancy):
+        pass
+
+    def delete_vacancy(self, vacancy: Vacancy):
+        pass
