e6337ff2b18be24562269c6e08b6b93e2112f5d2
Gammpyr <malak93@yandex.ru>
2025-06-24 15:52:43 +0300

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,61 @@
 import pytest
 
+from src.vacancy import Vacancy
+
+@pytest.fixture
+def data_test():
+    return [
+        Vacancy('','','', {'from': 20000},''),
+        Vacancy('','','', {'from': 10000},''),
+        Vacancy('','','', {'from': 30000},'')]
+
+
+@pytest.fixture
+def fake_vacancy():
+    return Vacancy('Тестировщик',
+                   'www.test.err',
+                   'Errors',
+                   {'from': 1, 'to':10},
+                   'Find and resolve errors')
+
+@pytest.fixture
+def fake_vacancy2():
+    return Vacancy('Тестировщик2',
+                   'www.test.err2',
+                   'Errors2',
+                   {'from': 2, 'to':20},
+                   'Find and resolve errors2')
+
+@pytest.fixture
+def fake_vacancy3():
+    return Vacancy('Тестировщик3',
+                   'www.test.err3',
+                   'Errors3',
+                   {'from': 2, 'to':30},
+                   'Find and resolve errors3')
+
+@pytest.fixture
+def fake_vacancy4():
+    return Vacancy('None',
+                   'None',
+                   'None',
+                   None,
+                   'None')
+
+@pytest.fixture
+def fake_request_dict():
+    result = {"name": "Manual QA Engineer",
+                 "salary": {
+                     "from": 80000,
+                     "to": 120000,
+                     "currency": "RUR",
+                     "gross": False
+                 },
+                 "alternate_url": "https://hh.ru/vacancy/121726350",
+                 "snippet": {
+                     "responsibility": "Тестирование web/backend. Написание и актуализация тестовой документации. Регистрация дефектов и контроль их устранения. Проверка реализованых задач на деве. "},
+                 "experience": {"id": "between1And3", "name": "От 1 года до 3 лет"}
+                 }
 
 @pytest.fixture
 def fake_request():
--- tests/test_file_workers.py
+++ tests/test_file_workers.py
@@ -1,9 +1,11 @@
 import json
-from unittest.mock import patch, mock_open, MagicMock
+from unittest.mock import patch, mock_open, MagicMock, Mock
 
 import pytest
 
 from src.file_workers import JSONSaver
+from src.vacancy import Vacancy
+from tests.conftest import fake_request
 
 json_file = '{"operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"},},}'
 json_saver = JSONSaver()
@@ -33,6 +35,18 @@ def test_delete_vacancy_error():
         with patch('builtins.print') as mock_print:
             json_saver.delete_vacancy("fake_file_name")
             mock_print.assert_called_once_with("Такого файла не существует")
+
+
+# @patch('src.file_workers.JSONSaver.get_data_from_file')
+# def test_add_vacancy(mock_data, fake_vacancy, fake_request):
+#     mock_data = MagicMock()
+#     mock_data.return_value = fake_request
+#
+#     json_saver.add_vacancy(fake_vacancy, 'fake_file')
+
+
+
+
 
 
 
